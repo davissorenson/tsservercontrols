4999d7eba0a1c3893fea5550070148c6ba7b45a8
Davis Sorenson <davis.sorenson@smartly.io>
2020-08-14 22:12:57 +0300

    Add project-wide typechecking toggle button

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # TypeScript Server Controls
 
-Restart the TypeScript server with a single click using a button in the status bar.
+Restart the TypeScript server or toggle project-wide typechecking with a single
+click
 
 ## Release Notes
 
-Users appreciate release notes as you update your extension.
+### 1.0
+
+Add project-wide typechecking toggle button. The TypeScript server's
+experimental "project diagnostics" option is very useful, but is very
+resource-intensive. Now you can quickly enable or disable it from the status
+bar next to the TS Server Restart button.
 
 ### 0.2.0
 
--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,56 @@
 import * as vscode from "vscode";
 import { StatusBarAlignment } from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export const activate = () => {
+const checkAllTSFilesConfigSection =
+  "typescript.tsserver.experimental.enableProjectDiagnostics";
+
+const getCurrentCheckAllTSFilesValue = (
+  userConfig: vscode.WorkspaceConfiguration
+): boolean => {
+  return !!userConfig.get(checkAllTSFilesConfigSection);
+};
+
+const toggleCheckAllTSFilesCommand = "tsservercontrols.toggleCheckAllTSFiles";
+
+const toggleCheckAllTSFilesHandler = () => {
+  const userConfig = vscode.workspace.getConfiguration();
+  const currentCheckAllTSFilesValue = getCurrentCheckAllTSFilesValue(
+    userConfig
+  );
+
+  userConfig.update(
+    checkAllTSFilesConfigSection,
+    !currentCheckAllTSFilesValue,
+    vscode.ConfigurationTarget.Global
+  );
+
+  vscode.commands.executeCommand(
+    showToggleCheckAllTSFilesButtonCommand,
+    !currentCheckAllTSFilesValue
+  );
+};
+
+const toggleCheckAllTSFilesButton = vscode.window.createStatusBarItem(
+  StatusBarAlignment.Left,
+  999_999
+);
+
+const showToggleCheckAllTSFilesButtonCommand =
+  "tsservercontrols.showToggleCheckAllTSFilesButton";
+
+const showToggleCheckAllTSFilesButtonHandler = (newValue?: boolean) => {
+  const userConfig = vscode.workspace.getConfiguration();
+  const currentCheckAllTSFilesValue =
+    newValue ?? getCurrentCheckAllTSFilesValue(userConfig);
+
+  toggleCheckAllTSFilesButton.command = toggleCheckAllTSFilesCommand;
+  toggleCheckAllTSFilesButton.text = currentCheckAllTSFilesValue
+    ? "$(debug-stop) Typechecking All"
+    : "$(play) Typechecking Current File";
+  toggleCheckAllTSFilesButton.show();
+};
+
+export const activate = (context: vscode.ExtensionContext) => {
   const restartTSServerButton = vscode.window.createStatusBarItem(
     StatusBarAlignment.Left,
     1_000_000
@@ -14,4 +61,20 @@ export const activate = () => {
   restartTSServerButton.command = "typescript.restartTsServer";
   restartTSServerButton.text = "$(refresh) Restart TS Server";
   restartTSServerButton.show();
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      toggleCheckAllTSFilesCommand,
+      toggleCheckAllTSFilesHandler
+    )
+  );
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      showToggleCheckAllTSFilesButtonCommand,
+      showToggleCheckAllTSFilesButtonHandler
+    )
+  );
+
+  vscode.commands.executeCommand(showToggleCheckAllTSFilesButtonCommand);
 };
